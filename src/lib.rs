52193cb4ecd69d7461c957cefe0d7a587ab0e8b2
Caleb Bourg <calebbourg2@gmail.com>
2022-06-19 12:34:07 -0700

    Set up Cargo doc
    Adds some Cargo.toml configuration
    as well as a main() function to build.rs. The crate will now compile and generate documentation.

---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+//! esp32-wroom-rp
+//! 
+//! Rust-based Espressif ESP32-WROOM WiFi hardware abstraction layer for RP2040 series microcontroller.
+//! Supports the [ESP32-WROOM-32E](https://www.espressif.com/sites/default/files/documentation/esp32-wroom-32e_esp32-wroom-32ue_datasheet_en.pdf), [ESP32-WROOM-32UE](https://www.espressif.com/sites/default/files/documentation/esp32-wroom-32e_esp32-wroom-32ue_datasheet_en.pdf) modules.
+//! Future implementations will support the [ESP32-WROOM-DA](https://www.espressif.com/sites/default/files/documentation/esp32-wroom-da_datasheet_en.pdf) module.
+//! 
+//! NOTE This crate is still under active development. This API will remain volatile until 1.0.0
