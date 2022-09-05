42b2fc8903322f1a6950e692fb180b66a5a67db7
Jim Hodapp <james.hodapp@gmail.com>
2022-09-05 12:52:57 -0500

    Removes old version of secrets.rs

--- examples/secrets.rs
+++
@@ -1,9 +0,0 @@
-//
-// secrets.rs - stores WiFi secrets like SSID, passphrase, etc shared across
-//              all example applications
-//
-
-use heapless::String;
-
-const SSID: &str = "ssid";
-const PASSPHRASE: &str = "passphrase";
