86ae2269e27b26c3303d2934676a4a2308d5de44
Caleb Bourg <calebbourg2@gmail.com>
2022-09-16 14:58:15 -0700

    add documentation to operation module

--- esp32-wroom-rp/src/protocol/operation.rs
+++ esp32-wroom-rp/src/protocol/operation.rs
@@ -3,6 +3,9 @@ use super::protocol::{NinaCommand, NinaNoParams, NinaParam};
 use heapless::Vec;
 const MAX_NUMBER_OF_PARAMS: usize = 4;
 
+// Encapsulates all information needed to execute commands against Nina Firmware.
+// along with user supplied data. Ex. SSID, passphrase, etc.
+
 pub struct Operation<P> {
     pub params: Vec<P, MAX_NUMBER_OF_PARAMS>,
     pub command: NinaCommand,
@@ -11,6 +14,9 @@ pub struct Operation<P> {
 }
 
 impl<P> Operation<P> {
+    // Initializes new Operation instance.
+    //
+    // `has_params` defaults to `true`
     pub fn new(command: NinaCommand, number_of_params_to_receive: u8) -> Self {
         Self {
             params: Vec::new(),
@@ -20,11 +26,15 @@ impl<P> Operation<P> {
         }
     }
 
+    // Pushes a new param into the internal `params` Vector.
     pub fn param(mut self, param: P) -> Self {
         self.params.push(param);
         self
     }
 
+    // Used for denoting an Operation where no params are necessary.
+    //
+    // Sets `has_params` to `false`
     pub fn with_no_params(mut self, no_param: P) -> Self {
         self.params.push(no_param);
         self.has_params = false;
