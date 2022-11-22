580cd0e4bdfd4ab63aaf7c41760cf134d8d1375d
Dilyn Corner <dilyn.corner@canonical.com>
2022-11-21 19:30:49 -0500

    Create and use the ConnectionStatus enum

--- esp32-wroom-rp/src/wifi.rs
+++ esp32-wroom-rp/src/wifi.rs
@@ -1,1 +1,18 @@
 pub use crate::spi::Wifi;
+
+/// Defines the WiFi network connection status
+#[repr(u8)]
+#[derive(Eq, PartialEq, PartialOrd, Debug)]
+pub enum ConnectionStatus {
+    NoEsp32 = 255,
+    Idle = 0, // Assigned by WiFi.begin() in the Reference Library, is this relevant?
+    NoActiveSsid,
+    ScanCompleted,
+    Connected,
+    Failed,
+    Lost,
+    Disconnected,
+    ApListening,
+    ApConnected,
+    ApFailed, // Not in the Reference Library
+}
